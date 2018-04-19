04c1867318ab1aedc888fb3e471435cebe0b3f88
gaufung <gaufung@outlook.com>
2018-04-19 17:08:57 +0800

    fix repl bug

--- token/token.go
+++ token/token.go
@@ -6,3 +6,50 @@ type Token struct {
 	Type TokenType
 	Literal string
 }
+
+const (
+	IllEGAL = "ILLEGAL"
+	EOF = "EOF"
+	IDENT = "IDENT"
+	INT = "INT"
+	ASSIGN = "="
+	PLUS = "+"
+	COMMA = ","
+	SEMICOLON = ";"
+	MINUS = "-"
+	BANG = "!"
+	ASTERISK="*"
+	SLASH = "/"
+	LT = "<"
+	GT = ">"
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+	FUNCTIOIN = "FUNCTION"
+	LET = "LET"
+	TRUE = "TRUE"
+	FALSE="FALSE"
+	IF="IF"
+	ELSE="ELSE"
+	RETURN="RETURN"
+	EQ = "=="
+	NOT_EQ = "!="
+)
+
+var keywords = map[string]TokenType{
+	"fn" : FUNCTIOIN,
+	"let" : LET,
+	"true" : TRUE,
+	"false" : FALSE,
+	"if" : IF,
+	"else":ELSE,
+	"return":RETURN,
+}
+
+func LookUpIdent(ident string) TokenType{
+	if tok, ok := keywords[ident]; ok{
+		return tok
+	}
+	return IDENT
+}
