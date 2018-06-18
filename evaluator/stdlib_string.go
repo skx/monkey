380c5ba590faf3a9e09e4ca53c62d8006e167f8c
Steve Kemp <steve@steve.org.uk>
2018-06-18 05:34:43 +0300

    Added string.interpolate.
    This is useful for expanding values into strings, and it
    is pretty flexible for doing that.

--- evaluator/stdlib_string.go
+++ evaluator/stdlib_string.go
@@ -2,8 +2,45 @@ package evaluator
 
 import (
 	"monkey/object"
+	"regexp"
 	"strings"
 )
+
+// string = string.interpolate( string, hash );
+func stringInterpolate(args ...object.Object) object.Object {
+	if len(args) != 2 {
+		return newError("wrong number of arguments. got=%d, want=1",
+			len(args))
+	}
+	if args[0].Type() != object.STRING_OBJ {
+		return newError("first argument must be a string, got=%s",
+			args[0].Type())
+	}
+	if args[1].Type() != object.HASH_OBJ {
+		return newError("second argument must be a string, got=%s",
+			args[0].Type())
+	}
+
+	str := args[0].(*object.String).Value
+	hashObject := args[1].(*object.Hash)
+
+	re := regexp.MustCompile("\\$\\{([^\\}]+)\\}")
+	out := re.ReplaceAllStringFunc(str, func(in string) string {
+
+		in = strings.TrimPrefix(in, "${")
+		in = strings.TrimSuffix(in, "}")
+
+		tmp := &object.String{Value: in}
+		key := tmp.HashKey()
+		k, ok := hashObject.Pairs[key]
+		if ok {
+			return (k.Value.Inspect())
+		} else {
+			return "${" + in + "}"
+		}
+	})
+	return &object.String{Value: out}
+}
 
 func stringToUpper(args ...object.Object) object.Object {
 	if len(args) != 1 {
@@ -80,6 +117,11 @@ func stringSplit(args ...object.Object) object.Object {
 }
 
 func init() {
+
+	registerBuiltin("string.interpolate",
+		func(args ...object.Object) object.Object {
+			return (stringInterpolate(args...))
+		})
 	registerBuiltin("string.toupper",
 		func(args ...object.Object) object.Object {
 			return (stringToUpper(args...))
