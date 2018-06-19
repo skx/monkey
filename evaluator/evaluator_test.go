912191a5b4c899333dd199084391e80927ce7ad4
Steve Kemp <steve@steve.org.uk>
2018-06-19 12:27:30 +0300

    Added more simple increment/decrement tests

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -263,6 +263,8 @@ func TestLetStatements(t *testing.T) {
 		{"let a=5;a;", 5},
 		{"let a=5*5; a;", 25},
 		{"let a=5; let b=a; b;", 5},
+		{"let a=5; a--; a;", 4},
+		{"let a=5; a++; a;", 6},
 		{"let a=5; let b=a; let c=a+b+5; c;", 15},
 	}
 	for _, tt := range tests {
