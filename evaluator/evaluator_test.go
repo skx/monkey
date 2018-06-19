60afc73bc4c82044aacdfae536e75df2432c90e1
Steve Kemp <steve@steve.org.uk>
2018-06-19 12:24:00 +0300

    Added tests cases for less-than-or-equal + greater-than-or-equal

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -128,6 +128,8 @@ func TestEvalBooleanExpression(t *testing.T) {
 		{"(1<2) == false", false},
 		{"(1>2) == true", false},
 		{"(1>2)==false", true},
+		{"(1>=1)==true", true},
+		{"(2<=2)==true", true},
 	}
 	for _, tt := range tests {
 		evaluated := testEval(tt.input)
