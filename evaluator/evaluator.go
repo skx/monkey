21653a40beba9ada73534ab2f28745fc92e58497
Steve Kemp <steve@steve.org.uk>
2018-10-05 06:25:08 +0300

    Better handling for boolean infix operators.
    We'd previously broken our tests by reusing the string-handler
    which implemented "+" , "+=", etc.  When all we really wanted
    to allow was <, <=, >, >=.
    
    This updates #45, by fixing the issue in a smaller and more
    specific way, and reinstating our working test-cases.

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -278,16 +278,31 @@ func evalInfixExpression(operator string, left, right object.Object) object.Obje
 	case operator == "!=":
 		return nativeBoolToBooleanObject(left != right)
 	case left.Type() == object.BOOLEAN_OBJ && right.Type() == object.BOOLEAN_OBJ:
-		// convert the bools to strings.
-		l := &object.String{Value: string(left.Inspect())}
-		r := &object.String{Value: string(right.Inspect())}
-
-		// This is a bit icky, but allows "<=", "<", etc to
-		// "work" for booleans.
-		return evalStringInfixExpression(operator, l, r)
+		return evalBooleanInfixExpression(operator, left, right)
 	case left.Type() != right.Type():
 		return newError("type mismatch: %s %s %s",
 			left.Type(), operator, right.Type())
+	default:
+		return newError("unknown operator: %s %s %s",
+			left.Type(), operator, right.Type())
+	}
+}
+
+// boolean operations
+func evalBooleanInfixExpression(operator string, left, right object.Object) object.Object {
+	// convert the bools to strings.
+	l := &object.String{Value: string(left.Inspect())}
+	r := &object.String{Value: string(right.Inspect())}
+
+	switch operator {
+	case "<":
+		return evalStringInfixExpression(operator, l, r)
+	case "<=":
+		return evalStringInfixExpression(operator, l, r)
+	case ">":
+		return evalStringInfixExpression(operator, l, r)
+	case ">=":
+		return evalStringInfixExpression(operator, l, r)
 	default:
 		return newError("unknown operator: %s %s %s",
 			left.Type(), operator, right.Type())
