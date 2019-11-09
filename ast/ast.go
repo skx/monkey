bfc98b119140f2b516bb9e054954aa4a3ef7248c
Steve Kemp <steve@steve.org.uk>
2019-11-09 06:23:28 +0200

    Added AST node for regexp

--- ast/ast.go
+++ ast/ast.go
@@ -561,6 +561,36 @@ func (sl *StringLiteral) TokenLiteral() string { return sl.Token.Literal }
 // String returns this object as a string.
 func (sl *StringLiteral) String() string { return sl.Token.Literal }
 
+// RegexpLiteral holds a regular-expression.
+type RegexpLiteral struct {
+	// Token is the token
+	Token token.Token
+
+	// Value is the value of the regular expression.
+	Value string
+}
+
+func (rl *RegexpLiteral) expressionNode() {}
+
+// TokenLiteral returns the literal token.
+func (rl *RegexpLiteral) TokenLiteral() string { return rl.Token.Literal }
+
+// String returns this object as a string.
+func (rl *RegexpLiteral) String() string {
+
+	start := "/"
+	val := rl.Token.Literal
+	end := "/"
+
+	if strings.HasPrefix(rl.Token.Literal, "(?i)") {
+		end = "/i"
+		val = strings.TrimPrefix(val, "(?i)")
+	}
+
+	str := start + val + end
+	return str
+}
+
 // BacktickLiteral holds details of a command to be executed
 type BacktickLiteral struct {
 	// Token is the actual token
